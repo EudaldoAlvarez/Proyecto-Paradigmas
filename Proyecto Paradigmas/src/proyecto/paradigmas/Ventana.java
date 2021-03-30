6c72af1028bf2e41aeecaf7a45b6370011c09859
EudaldoAlvarez <71477511+EudaldoAlvarez@users.noreply.github.com>
2021-03-30 15:25:00 -0700

    Problema de fondo solucionado

--- Proyecto Paradigmas/src/proyecto/paradigmas/Ventana.java
+++ Proyecto Paradigmas/src/proyecto/paradigmas/Ventana.java
@@ -5,71 +5,121 @@
  */
 package proyecto.paradigmas;
 
+import java.awt.Color;
 import java.awt.Dimension;
+import java.awt.Font;
+import java.awt.Graphics;
 import java.awt.GridLayout;
+import java.awt.Image;
 import java.awt.Toolkit;
 import javax.swing.ImageIcon;
 import javax.swing.JButton;
+import javax.swing.JComboBox;
 import javax.swing.JFrame;
 import javax.swing.JLabel;
 import javax.swing.JPanel;
+import javax.swing.JTextField;
 
 /**
  *
  * @author eudal
  */
 public class Ventana extends JFrame {
-
+    
+    private FondoPanel fondo =new FondoPanel();
     private int largo = 563, ancho = 1000;
-
+    
     public Ventana() {
         //===================Configuraciones basicas de la ventana=============
         setLayout(null);
         JLabel texto = new JLabel();
         Toolkit theKit = texto.getToolkit();
         Dimension pantalla = theKit.getScreenSize();
-
-        setLocation(((pantalla.width / 2) - (ancho / 2)), ((pantalla.height / 2) - (largo / 2)));
-        setSize(ancho, largo);
+        setBounds(((pantalla.width / 2) - (ancho / 2)),((pantalla.height / 2) - (largo / 2)),ancho,largo);
+        this.setContentPane(fondo);
+        
         setVisible(true);
-        setResizable(true);
-
+        setResizable(false);
+        
+        
         setDefaultCloseOperation(EXIT_ON_CLOSE);
-        //===================Configuraciones fin==========================
+        //===================Configuraciones basicas de la ventana fin==========================
 
-        //===================Contenedor principal==============================
-        JPanel contenedorPrincipal=new JPanel();
-        contenedorPrincipal.setBounds(0, 0, ancho, largo);
-        contenedorPrincipal.setLayout(new GridLayout(1, 2));
-        ImageIcon image=new ImageIcon("../img/bg.png");
-        JLabel bg=new JLabel();
-        bg.setSize(ancho, largo);
-        bg.setOpaque(true);
-        bg.setIcon(image);
-        contenedorPrincipal.add(bg);
-        add(contenedorPrincipal);
-        //===================Contenedor principa Fin
+        //==================Formulario==============================
+        JPanel formulario=new JPanel();
+        formulario.setOpaque(false);
+        formulario.setLayout(null);
+        formulario.setBounds(0,0,ancho/2,largo);
+        Font font=new Font("Bodoni MT Black",20, 20);
         
-        //===================Contenedor izquierdo===================
-        JPanel contenedorIzquierdo = new JPanel();
-        contenedorIzquierdo.setSize(ancho / 2, largo / 2);
-        //contenedorIzquierdo.setBackground(Color.red);
-        contenedorIzquierdo.setLayout(null);
+        JLabel nombre=new JLabel("Nombre");
+        nombre.setBounds(40, 50, 200, 40);
+        nombre.setFont(font);
+        nombre.setForeground(Color.white);
+        nombre.setOpaque(false);
+        formulario.add(nombre);
         
-        JButton hombre=new JButton();
-        hombre.setText("Hombre");
+        JTextField sendNombre=new JTextField();
+        sendNombre.setBounds(40, 100, 200, 40);
+        sendNombre.setFont(font);
+        sendNombre.setOpaque(true);
+        formulario.add(sendNombre);
+        
+        JLabel sexo=new JLabel("Sexo");
+        sexo.setBounds(40, 140, 70, 40);
+        sexo.setFont(font);
+        sexo.setForeground(Color.white);
+        formulario.add(sexo);
+        
+        JComboBox sendSexo=new JComboBox();
+        sendSexo.setBounds(110,145, 30, 30);
+        sendSexo.setFont(font);
+        sendSexo.setBackground(Color.white);
+        sendSexo.setOpaque(false);
+        sendSexo.addItem("H");
+        sendSexo.addItem("M");
+        formulario.add(sendSexo);
+        
+        JLabel peso=new JLabel("Peso(Kg)");
+        peso.setBounds(40, 180, 70, 40);
+        peso.setFont(font);
+        peso.setForeground(Color.white);
+        formulario.add(peso);
+        
+        JTextField sendPeso=new JTextField();
+        sendPeso.setBounds(95,180, 50, 40);
+        
+        sendPeso.setFont(font);
+        sendPeso.setOpaque(true);
+        formulario.add(sendPeso);
+        
+        JLabel altura=new JLabel("Altura(mts)");
+        altura.setBounds(40, 225, 200, 40);
+        altura.setFont(font);
+        altura.setForeground(Color.white);
+        formulario.add(altura);
+        
+        JTextField sendAltura=new JTextField();
+        sendAltura.setBounds(40, 265, 200, 40);
+        sendAltura.setFont(font);
+        sendAltura.setOpaque(true);
+        formulario.add(sendAltura);
+        
+        add(formulario);
+        repaint();
+        //==================Formulario Fin==========================
         
         
-        contenedorPrincipal.add(contenedorIzquierdo);
-        //===================Contedor izquierdo fin===================
-
-        //===================Contendor Derecho========================
-        JPanel contenedorDerecho = new JPanel();
-        contenedorDerecho.setSize(ancho / 2, largo / 2);
-        //contenedorDerecho.setBackground(Color.blue);
-        contenedorPrincipal.add(contenedorDerecho);
-        //===================Contenedor Derecho fin===================
 
     }
-
+    class FondoPanel extends JPanel{
+        private Image imagen;
+        @Override
+        public void paint(Graphics g){
+          imagen = new ImageIcon(getClass().getResource("/img/bg.png")).getImage();
+          g.drawImage(imagen, 0, 0, getWidth(),getHeight(),this);
+          setOpaque(false);
+          super.paint(g);
+        }
+    }
 }
